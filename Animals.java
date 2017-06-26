d5dcda383e9a27c6c675f5cf99cf02f7f500d818
Alena Golovina and Joel Bakken <joel@>
2017-06-26 16:34:44 -0700

    add custom method for adopted false to true

--- Animals.java
+++ Animals.java
@@ -5,13 +5,26 @@ class Animals {
   public String mName;
   public int mPrice;
   public String mSize;
-  public boolean mIsAvailable;
+  public boolean mAdopted;
 
+
+  public Animals(String species, String breed, int age, String name, int price, String size, boolean adopted) {
+    mSpecies = species;
+    mBreed = breed;
+    mAge = age;
+    mName = name;
+    mPrice = price;
+    mSize = size;
+    mAdopted = adopted;
+  }
 
   public boolean worthBuying(int maxPrice) {
     return (mPrice < maxPrice);
   }
   public boolean insidePet() {
     return (mSize.equals("small") || mSize.equals("medium"));
+  }
+  public boolean adoptedPet() {
+    return (mAdopted = true);
   }
 }
--- App.java
+++ App.java
@@ -4,53 +4,57 @@ public class App {
  public static void main(String[] args) {
    Console myConsole = System.console();
 
-   Animals cat = new Animals();
-      cat.mSpecies = "Cat";
-      cat.mBreed = "Maincoon";
-      cat.mAge = 2;
-      cat.mName = "Pushok";
-      cat.mPrice = 500;
-      cat.mSize = "small";
-      cat.mIsAvailable = true;
 
-    Animals dog = new Animals();
-     dog.mSpecies = "Dog";
-     dog.mBreed = "Terrier";
-     dog.mAge = 3;
-     dog.mName = "Druzhok";
-     dog.mPrice = 100;
-     dog.mSize = "medium";
-     dog.mIsAvailable = true;
+      Animals cat = new Animals("Cat", "Maincoon", 2, "Pushok", 500, "small", false);
+      Animals dog = new Animals("Dog", "Terrier", 3, "Druzhok", 100, "medium", false);
+      Animals monkey = new Animals("Monkey", "Chimpanzee", 15, "Billy", 1000, "big", false);
+  //  Animals cat = new Animals();
+  //     cat.mSpecies = "Cat";
+  //     cat.mBreed = "Maincoon";
+  //     cat.mAge = 2;
+  //     cat.mName = "Pushok";
+  //     cat.mPrice = 500;
+  //     cat.mSize = "small";
+  //     cat.mAdopted = false;
+   //
+  //   Animals dog = new Animals();
+  //    dog.mSpecies = "Dog";
+  //    dog.mBreed = "Terrier";
+  //    dog.mAge = 3;
+  //    dog.mName = "Druzhok";
+  //    dog.mPrice = 100;
+  //    dog.mSize = "medium";
+  //    dog.mAdopted = false;
+   //
+  //   Animals monkey = new Animals();
+  //     monkey.mSpecies = "Monkey";
+  //     monkey.mBreed = "Chimpanzee";
+  //     monkey.mAge = 15;
+  //     monkey.mName = "Billy";
+  //     monkey.mPrice = 1000;
+  //     monkey.mSize = "big";
+  //     monkey.mAdopted = false;
+   //
+  //   Animals horse = new Animals();
+  //     horse.mSpecies = "Horse";
+  //     horse.mBreed = "Pony";
+  //     horse.mAge = 7;
+  //     horse.mName = "Pickles";
+  //     horse.mPrice = 10000;
+  //     horse.mSize = "big";
+  //     horse.mAdopted = false;
+   //
+  //   Animals parrot = new Animals();
+  //     parrot.mSpecies = "Parrot";
+  //     parrot.mBreed = "Ara";
+  //     parrot.mAge = 1;
+  //     parrot.mName = "Gosha";
+  //     parrot.mPrice = 120;
+  //     parrot.mSize = "small";
+  //     parrot.mAdopted = false;
 
-    Animals monkey = new Animals();
-      monkey.mSpecies = "Monkey";
-      monkey.mBreed = "Chimpanzee";
-      monkey.mAge = 15;
-      monkey.mName = "Billy";
-      monkey.mPrice = 1000;
-      monkey.mSize = "big";
-      monkey.mIsAvailable = true;
 
-    Animals horse = new Animals();
-      horse.mSpecies = "Horse";
-      horse.mBreed = "Pony";
-      horse.mAge = 7;
-      horse.mName = "Pickles";
-      horse.mPrice = 10000;
-      horse.mSize = "big";
-      horse.mIsAvailable = true;
-
-    Animals parrot = new Animals();
-      parrot.mSpecies = "Parrot";
-      parrot.mBreed = "Ara";
-      parrot.mAge = 1;
-      parrot.mName = "Gosha";
-      parrot.mPrice = 120;
-      parrot.mSize = "small";
-      parrot.mIsAvailable = false;
-
-
-      Animals[] allAnimals = {cat, dog, monkey, horse, parrot};
+      Animals[] allAnimals = {cat, dog, monkey};
 
       System.out.println("What is your maximum budget for a pet?");
       String stringUserMaxBudget = myConsole.readLine();
@@ -69,11 +73,15 @@ public class App {
           System.out.println( individualAnimals.mAge );
           System.out.println( individualAnimals.mName );
           System.out.println( individualAnimals.mPrice );
-          System.out.println( individualAnimals.mIsAvailable );
+          // System.out.println( individualAnimals.mAdopted );
           if (individualAnimals.insidePet()) {
             System.out.println("This is an inside animal");
+          }
+          if (individualAnimals.adoptedPet()) {
+              System.out.println( individualAnimals.mAdopted );
+           }
+
           }
         }
       }
   }
-}
